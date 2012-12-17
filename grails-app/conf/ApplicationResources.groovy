308d30c0829d174da23b957e10c8e173e26bd5d4
Paul Citarella <pc@paulcitarella.com>
2012-12-17 15:05:16 -0500

    Cleanup

--- grails-app/conf/ApplicationResources.groovy
+++ grails-app/conf/ApplicationResources.groovy
@@ -3,8 +3,8 @@ modules = {
         defaultBundle 'monolith'
         dependsOn 'jquery', 'bootstrap'
         resource url: 'css/scaffolding.css'
-        resource url:'js/application.js'
-        resource url:'less/application.less', attrs:[rel: "stylesheet/less", type:'css'], bundle:'monolith'
+        resource url: 'js/application.js'
+        resource url: 'less/application.less', attrs:[rel: "stylesheet/less", type:'css'], bundle:'monolith'
         resource url: 'css/dummy.css'
     }
 
