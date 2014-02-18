bd66566a82aa920e8b1115cb53cdc83e8ad8fc63
Paul Citarella <pc@paulcitarella.com>
2014-02-18 10:36:48 -0500

    Initial bootstrap 3 resources

--- grails-app/conf/BootstrapResources.groovy
+++ grails-app/conf/BootstrapResources.groovy
@@ -1,86 +1,80 @@
 modules = {
 
-    'bootstrap-alert' {
-        defaultBundle 'monolith'
-        dependsOn 'jquery'
-        resource url:[dir: 'bootstrap/js', file: 'bootstrap-alert.js']
-    }
-
     'bootstrap-affix' {
         defaultBundle 'monolith'
         dependsOn 'jquery'
-        resource url:[dir: 'bootstrap/js', file: 'bootstrap-affix.js']
+        resource url:[dir: 'bootstrap/js', file: 'affix.js']
     }
 
-    'bootstrap-dropdown' {
+    'bootstrap-alert' {
         defaultBundle 'monolith'
         dependsOn 'jquery'
-        resource url:[dir: 'bootstrap/js', file: 'bootstrap-dropdown.js']
-    }
-
-    'bootstrap-modal' {
-        defaultBundle 'monolith'
-        dependsOn 'jquery'
-        resource url:[dir: 'bootstrap/js', file: 'bootstrap-modal.js']
-    }
-
-    'bootstrap-popover' {
-        defaultBundle 'monolith'
-        dependsOn 'bootstrap-tooltip, jquery'
-        resource url:[dir: 'bootstrap/js', file: 'bootstrap-popover.js']
-    }
-
-    'bootstrap-scrollspy' {
-        defaultBundle 'monolith'
-        dependsOn 'jquery'
-        resource url:[dir: 'bootstrap/js', file: 'bootstrap-scrollspy.js']
-    }
-
-    'bootstrap-tab' {
-        defaultBundle 'monolith'
-        dependsOn 'jquery'
-        resource url:[dir: 'bootstrap/js', file: 'bootstrap-tab.js']
-    }
-
-    'bootstrap-tooltip' {
-        defaultBundle 'monolith'
-        dependsOn 'jquery'
-        resource url:[dir: 'bootstrap/js', file: 'bootstrap-tooltip.js']
+        resource url:[dir: 'bootstrap/js', file: 'alert.js']
     }
 
     'bootstrap-button' {
         defaultBundle 'monolith'
         dependsOn 'jquery'
-        resource url:[dir: 'bootstrap/js', file: 'bootstrap-button.js']
+        resource url:[dir: 'bootstrap/js', file: 'button.js']
     }
 
     'bootstrap-carousel' {
         defaultBundle 'monolith'
         dependsOn 'jquery'
-        resource url:[dir: 'bootstrap/js', file: 'bootstrap-carousel.js']
-    }
-
-    'bootstrap-typeahead' {
-        defaultBundle 'monolith'
-        dependsOn 'jquery'
-        resource url:[dir: 'bootstrap/js', file: 'bootstrap-typeahead.js']
+        resource url:[dir: 'bootstrap/js', file: 'carousel.js']
     }
 
     'bootstrap-collapse' {
         defaultBundle 'monolith'
         dependsOn 'jquery'
-        resource url:[dir: 'bootstrap/js', file: 'bootstrap-collapse.js']
+        resource url:[dir: 'bootstrap/js', file: 'collapse.js']
+    }
+
+    'bootstrap-dropdown' {
+        defaultBundle 'monolith'
+        dependsOn 'jquery'
+        resource url:[dir: 'bootstrap/js', file: 'dropdown.js']
+    }
+
+    'bootstrap-modal' {
+        defaultBundle 'monolith'
+        dependsOn 'jquery'
+        resource url:[dir: 'bootstrap/js', file: 'modal.js']
+    }
+
+    'bootstrap-popover' {
+        defaultBundle 'monolith'
+        dependsOn 'jquery'
+        resource url:[dir: 'bootstrap/js', file: 'popover.js']
+    }
+
+    'bootstrap-scrollspy' {
+        defaultBundle 'monolith'
+        dependsOn 'jquery'
+        resource url:[dir: 'bootstrap/js', file: 'scrollspy.js']
+    }
+
+    'bootstrap-tab' {
+        defaultBundle 'monolith'
+        dependsOn 'jquery'
+        resource url:[dir: 'bootstrap/js', file: 'tab.js']
+    }
+
+    'bootstrap-tooltip' {
+        defaultBundle 'monolith'
+        dependsOn 'jquery'
+        resource url:[dir: 'bootstrap/js', file: 'tooltip.js']
     }
 
     'bootstrap-transition' {
         defaultBundle 'monolith'
-        dependsOn "jquery"
-        resource url:[dir: 'bootstrap/js', file: 'bootstrap-transition.js']
+        dependsOn 'jquery'
+        resource url:[dir: 'bootstrap/js', file: 'transition.js']
     }
 
     'bootstrap-js' {
         defaultBundle 'monolith'
         dependsOn 'jquery'
-        dependsOn 'bootstrap-transition, bootstrap-alert, bootstrap-dropdown, bootstrap-modal, bootstrap-scrollspy, bootstrap-tab, bootstrap-tooltip, bootstrap-popover, bootstrap-button, bootstrap-carousel, bootstrap-typeahead, bootstrap-collapse, bootstrap-affix'
+        dependsOn 'bootstrap-affix', 'bootstrap-alert', 'bootstrap-button', 'bootstrap-carousel', 'bootstrap-collapse', 'bootstrap-dropdown', 'bootstrap-modal', 'bootstrap-popover', 'bootstrap-scrollspy', 'bootstrap-tab', 'bootstrap-tooltip', 'bootstrap-transition'
     }
 }
